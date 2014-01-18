5cc0a085360ff4c0bc45b6b9576a95ab75287b3e
Francisco Adasme <francisco.adasme@gmail.com>
2014-01-18 15:34:40 -0300

    Removed announcements

--- app/mailers/default_mailer.rb
+++ app/mailers/default_mailer.rb
@@ -13,15 +13,4 @@ class DefaultMailer < ActionMailer::Base
 
     mail(to: email, subject: subject, template_path: 'mailer')
   end
-
-  def send_announcement(from, emails, subject, body)
-    @user = from
-    @content = body
-
-    @group = Group.first
-
-    mail(to: emails,
-         subject: "[Announcement] #{subject}",
-         template_path: 'mailer')
-  end
 end
