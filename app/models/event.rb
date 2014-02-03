a900fbf972e60fb384aa4b04e27436f836c5263b
Francisco Adasme <francisco.adasme@gmail.com>
2014-02-03 19:48:48 -0300

    Changed start_date validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -26,11 +26,13 @@ class Event < ActiveRecord::Base
     thumb: '320x320#'
   }
 
+  scope :sorted, -> { order start_date: :desc }
+
   auto_strip_attributes :name, :location, :description
   validates :name, presence: true,
                      length: { in: 4..128 }
   validates :start_date, presence: true,
-                       timeliness: { on_or_after: -> { Date.today }, on: :create }
+                       timeliness: true
   validates :end_date, timeliness: { on_or_after: :start_date },
                       allow_blank: true
   validates :location, presence: true
