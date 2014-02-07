a072e916470e9a00ba0fb7570314585491eaecf3
Francisco Adasme <francisco.adasme@gmail.com>
2014-02-07 20:08:20 -0300

    Added posts filters + custom header

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < SiteController
   def index
     @posts = Post.published.sorted
+    if params[:year]
+      date = Date.new params[:year].to_i, (params[:month] || 1).to_i
+      from_date = date.beginning_of_month
+      to_date = date.end_of_month
+      @posts = @posts.where created_at: from_date..to_date
+    end
   end
 
   def show
--- config/initializers/core_extensions.rb
+++ config/initializers/core_extensions.rb
@@ -78,6 +78,17 @@ class Hash
   end
 end
 
+class Range
+  def time_step(step, &block)
+    return enum_for(:time_step, step) unless block_given?
+
+    start_time, end_time = first, last
+    begin
+      yield(start_time)
+    end while (start_time += step) <= end_time
+  end
+end
+
 module ActiveSupport
   class MessageEncryptor
     def self.default_encryptor
