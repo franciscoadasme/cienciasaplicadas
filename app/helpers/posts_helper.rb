800bb82c988cdc3ede1e0540a8a2150a8530dec9
Francisco Adasme <francisco.adasme@gmail.com>
2014-02-08 15:56:47 -0300

    Added missing posts_this_month_path helper method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,9 @@ module PostsHelper
     date = post.created_at
     post_path id: post.slug, year: date.year, month: date.month, day: date.day
   end
+
+  def posts_this_month_path
+    today = DateTime.current
+    posts_path year: today.year, month: today.month
+  end
 end
