f6e4d7b4d02ae10d9671dd93077b256f7557191b
Francisco Adasme <francisco.adasme@gmail.com>
2014-02-08 15:54:16 -0300

    Fixed issue when post is nil

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,6 @@
 module PostsHelper
   def post_permalink(post)
+    return post if post.blank?
     date = post.created_at
     post_path id: post.slug, year: date.year, month: date.month, day: date.day
   end
