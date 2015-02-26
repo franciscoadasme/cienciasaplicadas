248bc65a3ad3fc9b8270ab3efa056c4e66b83afd
Francisco Adasme <francisco.adasme@gmail.com>
2015-02-26 19:12:43 -0300

    Added count to publication year sidebar nav

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -2,7 +2,7 @@ class PublicationsController < SiteController
   before_action :set_user
   def index
     @pubs = @user.publications.sorted
-    @years = @pubs.pluck(:year).uniq
+    @pubs_per_year = Hash[@pubs.group_by(&:year).map{ |y, ps| [y, ps.count] }]
   end
 
   private
