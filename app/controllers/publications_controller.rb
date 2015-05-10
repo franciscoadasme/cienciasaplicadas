0a2061ebcd0ad7e17e5fca9962d7531961d84fef
Francisco Adasme <francisco.adasme@gmail.com>
2015-05-10 20:07:04 -0300

    Fixed omitting empty years in publication graph

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -2,11 +2,12 @@ class PublicationsController < SiteController
   before_action :set_user
   def index
     @pubs = @user.publications.includes(:authors, :journal).sorted
-    @pubs_per_year = Hash[@pubs.group_by(&:year).map{ |y, ps| [y, ps.count] }]
+    @pubs_per_year = @user.statistics.publication_per_year.reverse
   end
 
   private
-    def set_user
-      @user = User.friendly.find params[:user_id]
-    end
+
+  def set_user
+    @user = User.friendly.find params[:user_id]
+  end
 end
--- app/value_objects/author_statistics.rb
+++ app/value_objects/author_statistics.rb
@@ -20,11 +20,22 @@ class AuthorStatistics
   end
 
   def publication_per_journal
-    Journal.joins(:publications).where('publications.id' => publication_ids).group(:name).count
+    Journal.joins(:publications)
+      .where('publications.id' => publication_ids)
+      .group(:name).count
   end
 
-  def publication_per_year
-    publication_group_by(:year).sort
+  def publication_per_year(include_empty_years: false)
+    @publications_per_year ||= publication_group_by(:year).sort
+    if include_empty_years
+      pubs_per_year = Array.new(@publications_per_year)
+      years = pubs_per_year.map { |year, _| year }
+      years[0].upto(Date.today.year) do |year|
+        pubs_per_year << [year, 0] unless years.include?(year)
+      end
+      return pubs_per_year.sort
+    end
+    @publications_per_year
   end
 
   def avg_publication_per_year
@@ -32,15 +43,16 @@ class AuthorStatistics
   end
 
   private
-    def journal_ids
-      @journal_ids ||= @user.publications.pluck(:journal_id)
-    end
 
-    def publication_group_by(*args)
-      @user.publications.group(args).count
-    end
+  def journal_ids
+    @journal_ids ||= @user.publications.pluck(:journal_id)
+  end
 
-    def publication_ids
-      @publication_ids ||= @user.publications.pluck(:id)
-    end
+  def publication_group_by(*args)
+    @user.publications.group(args).count
+  end
+
+  def publication_ids
+    @publication_ids ||= @user.publications.pluck(:id)
+  end
 end
