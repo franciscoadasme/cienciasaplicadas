91c83645768688d36b4b013f8b3c0f07ed979bda
Francisco Adasme <francisco.adasme@gmail.com>
2015-10-13 22:04:40 +0200

    Removed custom handling of user not found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 class UsersController < SiteController
   before_action :set_user
 
-  rescue_from ActiveRecord::RecordNotFound do
-    redirect_to :root, error: { action: :show, user: params[:user_id] }
-  end
-
   def show
     @user_pubs = @user.publications.flagged.sorted
     @user_pubs = @user.publications.sorted.limit(5) if @user_pubs.empty?
