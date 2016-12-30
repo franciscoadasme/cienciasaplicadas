6d31bb52b02204eaacb32862e100563961e15f8d
Francisco Adasme <francisco.adasme@gmail.com>
2016-12-30 14:13:50 -0300

    Refactored NotifiableController to reusable methods

--- app/controllers/concerns/notifiable_controller.rb
+++ app/controllers/concerns/notifiable_controller.rb
@@ -1,17 +1,22 @@
 module NotifiableController
   extend ActiveSupport::Concern
 
+  def position_ids
+    notification_params.fetch(:position_ids, []).reject(&:blank?)
+  end
+
   def notification_params
     params.require(:notification).permit position_ids: []
   end
 
   def send_new_notification_if_needed(record)
-    position_ids = notification_params.fetch(:position_ids, []).reject(&:blank?)
     return if position_ids.empty?
-
-    users = User.default.joins(:position)
-            .where('positions.id' => position_ids)
     notifier = "send_new_#{record.class.name.underscore}_notification"
-    NotificationMailer.send(notifier, record, users).deliver
+    NotificationMailer.send(notifier, record, notification_recipients).deliver
+  end
+
+  def notification_recipients
+    @recipients ||= User.default.joins(:position)
+                        .where('positions.id' => position_ids)
   end
 end
