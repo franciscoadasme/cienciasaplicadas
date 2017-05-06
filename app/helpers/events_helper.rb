87699eda1d287b6e9d012d3463320e8df2d31dca
Francisco Adasme <francisco.adasme@gmail.com>
2017-05-06 15:11:35 -0300

    Refactored date nav widget for events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,6 +3,16 @@ module EventsHelper
     image_url "events/#{event_type}.jpg"
   end
 
+  def event_nav_widget
+    today = DateTime.current.to_date
+    start_month = today.advance(months: -1)
+    end_month = today.advance(months: 6)
+    items = [
+      { title: I18n.t('views.nav.date.upcoming'), path: upcoming_events_path }
+    ]
+    nav_date_widget start_month, end_month, :events_path, 1.month, items
+  end
+
   def nav_header_data_for_event(event)
     data = {}
     data[:prev] = {
