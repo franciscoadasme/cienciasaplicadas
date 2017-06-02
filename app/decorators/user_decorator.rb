196ca4d1058e1f0a11871fe04bbebf13bd39df7c
Francisco Adasme <francisco.adasme@gmail.com>
2017-06-02 00:05:04 -0400

    Added a bunch of decorated user attributes

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -21,4 +21,31 @@ class UserDecorator < Draper::Decorator
   def link
     h.link_to object.display_name, h.user_url(object)
   end
+
+  def mail_link
+    return nil unless object.settings.show_contact_page
+    h.mail_to object.email do
+      h.fa_icon :'envelope-o', text: object.email
+    end
+  end
+
+  def position_badge
+    h.content_tag :span, object.position.name,
+                  class: 'badge badge-outline-primary'
+  end
+
+  def project_info
+    return nil unless object.projects.any?
+    I18n.t('views.users.single.project_info_html',
+           count: object.projects.count,
+           url: h.user_projects_url(object)).html_safe
+  end
+
+  def pub_info
+    return nil unless object.publications.any?
+    I18n.t('views.users.single.pub_info_html',
+           count: object.statistics.publication_total,
+           url: h.user_publications_url(object),
+           journals: object.statistics.journal_total).html_safe
+  end
 end
