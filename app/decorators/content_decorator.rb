faad006b29c0328a61ee8999211817d36b2c8e83
Francisco Adasme <francisco.adasme@gmail.com>
2017-06-03 23:45:22 -0400

    Improved user-created html formatting
    Now we force paragraphs everywhere, leverage nokogiri's power to get well-formed html (e.g., remove nested <p>s), and then strip empty paragraphs and remaining ending hard line breaks.

--- app/decorators/content_decorator.rb
+++ app/decorators/content_decorator.rb
@@ -1,10 +1,25 @@
 class ContentDecorator < Draper::Decorator
+  # TODO: avoid this hack to ensure well-formed html
   def body
-    object.body
-          .gsub(%r{(^<div>)|(<\/div>$)}, '') # remove enclosing div
-          .split(/\s*<br><br>\s*/) # replace double line breaks with p tags
-          .map { |text| h.content_tag(:p, text, nil, false) }.join('')
-          .gsub(/<br>(?=<)/, '') # remove ending line breaks
-          .html_safe
+    @body ||= paragraphify(object.body)
+              .gsub! %r{<(/?)h1>}, '<\1h2>'
+    @body.html_safe
+  end
+
+  private
+
+  def ensure_well_formed_html
+    Nokogiri::HTML.fragment(html).to_s.tap do |content|
+      content.gsub! %r{<p>(<br>)?</p>}, '' # remove empty paragraphs
+      content.gsub! %r{<br>\s*</p>}, '</p>' # remove newline from paragraph
+    end
+  end
+
+  def paragraphify(content)
+    content = content.gsub(/div>/, 'p>') # change <div> by <p>
+                     .split(/\s*<br><br>\s*/)
+                     .map { |text| h.content_tag(:p, text, nil, false) }
+                     .join('')
+    ensure_well_formed_html content
   end
 end
