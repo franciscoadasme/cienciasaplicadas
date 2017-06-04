c290c2da8650ab89511bcd08771fb43b40070a25
Francisco Adasme <francisco.adasme@gmail.com>
2017-06-04 14:42:55 -0400

    Added link method to post decorator

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -10,6 +10,11 @@ class PostDecorator < ContentDecorator
     h.excerpt(object.body, truncate_at: 160).html_safe
   end
 
+  def link(html_options = {}, &block)
+    return h.link_to permalink, html_options, &block if block_given?
+    h.link_to object.title, permalink, html_options
+  end
+
   def permalink
     date = object.created_at
     h.post_url id: object.slug, year: date.year, month: date.month, day: date.day
