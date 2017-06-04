7f0404bb5128d08cc8036e53d4def65bb182de64
Francisco Adasme <francisco.adasme@gmail.com>
2017-06-03 23:53:15 -0400

    Fixed some stupid typos

--- app/decorators/content_decorator.rb
+++ app/decorators/content_decorator.rb
@@ -2,16 +2,16 @@ class ContentDecorator < Draper::Decorator
   # TODO: avoid this hack to ensure well-formed html
   def body
     @body ||= paragraphify(object.body)
-              .gsub! %r{<(/?)h1>}, '<\1h2>'
+              .gsub %r{<(/?)h1>}, '<\1h2>'
     @body.html_safe
   end
 
   private
 
-  def ensure_well_formed_html
-    Nokogiri::HTML.fragment(html).to_s.tap do |content|
-      content.gsub! %r{<p>(<br>)?</p>}, '' # remove empty paragraphs
-      content.gsub! %r{<br>\s*</p>}, '</p>' # remove newline from paragraph
+  def ensure_well_formed_html(content)
+    Nokogiri::HTML.fragment(content).to_s.tap do |html|
+      html.gsub! %r{<p>(<br>)?</p>}, '' # remove empty paragraphs
+      html.gsub! %r{<br>\s*</p>}, '</p>' # remove newline from paragraph
     end
   end
 
