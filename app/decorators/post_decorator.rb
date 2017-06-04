3d09fa799cbb2caa288250c34b5a32807c89d0bd
Francisco Adasme <francisco.adasme@gmail.com>
2017-06-04 14:37:43 -0400

    Added permalink to post decorator

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -6,6 +6,11 @@ class PostDecorator < ContentDecorator
 
   delegate :link, to: :author, prefix: true
 
+  def permalink
+    date = object.created_at
+    h.post_url id: object.slug, year: date.year, month: date.month, day: date.day
+  end
+
   def published_at
     time_tag created_at,
              l(created_at, format: :abbr_with_day),
