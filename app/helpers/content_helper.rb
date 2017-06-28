593dc280b4ba090a69e5a1f05c3cb5a0778fdbbc
Francisco Adasme <francisco.adasme@gmail.com>
2017-06-27 23:25:24 -0400

    Ensure a space after each comma on excerpt

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,6 +1,8 @@
 # Methods to deal with user-created content (e.g., posts, pages)
 module ContentHelper
   def excerpt(content, truncate_at: 140, separator: /(?<=[a-z])\s+/, omission: '...')
-    strip_tags(content).truncate truncate_at, separator: separator, omission: omission
+    strip_tags(content)
+      .gsub(/,(?=[a-z])/i, ', ')
+      .truncate truncate_at, separator: separator, omission: omission
   end
 end
