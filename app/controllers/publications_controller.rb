a088d6c6b5964a684c378c17ef378f6a89d3e81c
Francisco Adasme <francisco.adasme@gmail.com>
2017-07-03 03:52:57 -0400

    Huge refactor of publications views

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,13 +1,16 @@
 class PublicationsController < SiteController
   before_action :set_user
+  decorates_assigned :publications, :user
+
   def index
-    @pubs = @user.publications.includes(:authors, :journal).sorted
+    @publications = @user.publications.includes(:authors, :journal).sorted
     @pubs_per_year = @user.statistics.publication_per_year.reverse
   end
 
   private
 
   def set_user
-    @user = User.friendly.find params[:user_id]
+    @user = User.includes(:projects, :publications, :thesis)
+                .friendly.find params[:user_id]
   end
 end
