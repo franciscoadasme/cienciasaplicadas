88ffa63dd42e1cc1dc408c32058955e95e7cd372
Francisco Adasme <francisco.adasme@gmail.com>
2017-07-16 20:49:19 -0400

    Added missing cases in EventDecorator.date_span
    Added abbr_with_day_no_year date format to locales

--- app/decorators/event_decorator.rb
+++ app/decorators/event_decorator.rb
@@ -2,11 +2,17 @@ class EventDecorator < ContentDecorator
   delegate_all
 
   def date_span
-    if one_day?
-      h.time_tag start_date, l(start_date, format: :abbr_with_day)
-    elsif start_date.month == end_date.month
-      content = "#{start_date.day}–#{l end_date, format: :abbr_with_day}"
-      h.time_tag start_date, content
-    end
+    content = if one_day?
+                l(start_date, format: :abbr_with_day)
+              elsif start_date.month == end_date.month
+                "#{start_date.day}–#{l end_date, format: :abbr_with_day}"
+              elsif start_date.year == end_date.year
+                "#{l start_date, format: :abbr_with_day_no_year}–" \
+                "#{l end_date, format: :abbr_with_day}"
+              else
+                "#{l start_date, format: :abbr_with_day}–" \
+                "#{l end_date, format: :abbr_with_day}"
+              end
+    h.time_tag start_date, content
   end
 end
