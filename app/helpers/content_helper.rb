363a35cf04cade14f5ebd0d441a3ad95654856da
Francisco Adasme <francisco.adasme@gmail.com>
2017-07-19 23:43:48 -0400

    Fixed not adding space afte dot on excerpts

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -2,7 +2,7 @@
 module ContentHelper
   def excerpt(content, truncate_at: 140, separator: /(?<=[a-z])\s+/, omission: '...')
     strip_tags(content)
-      .gsub(/,(?=[a-z])/i, ', ')
+      .gsub(/([\.,])([a-z])/i, '\1 \2')
       .truncate truncate_at, separator: separator, omission: omission
   end
 end
