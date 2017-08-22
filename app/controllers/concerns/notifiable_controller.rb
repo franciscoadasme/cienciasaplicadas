30db0bc63f64da584cecf18e6a9f79bd355b2915
Francisco Adasme <francisco.adasme@gmail.com>
2017-08-21 21:18:08 -0300

    Added event attendees' emails as notification target

--- app/controllers/concerns/notifiable_controller.rb
+++ app/controllers/concerns/notifiable_controller.rb
@@ -19,13 +19,20 @@ module NotifiableController
     @recipients ||= begin
       recipients = User.default.joins(:position)
                        .where('positions.id' => position_ids).to_a
+      # if notify_attendees? is true, then @post.event exists and it's not nil
+      recipients.concat @post.event.attendees if notify_attendees?
+      recipients
     end
   end
 
   private
 
+  def notify_attendees?
+    params[:notification].key?(:attendees)
+  end
+
   def send_notification?
     return false unless params.key?(:notification)
-    position_ids.any?
+    position_ids.any? || notify_attendees?
   end
 end
