0bc12627896db8d1653b131de756e515476fe6a7
Francisco Adasme <francisco.adasme@gmail.com>
2017-10-05 13:20:18 -0300

    Refactored abstract mailer methods

--- app/mailers/abstract_mailer.rb
+++ app/mailers/abstract_mailer.rb
@@ -1,14 +1,21 @@
 class AbstractMailer < DefaultMailer
   def submission_token(abstract)
+    notification :submission_token, abstract
+  end
+
+  private
+
+  def notification(notification_name, abstract)
     @abstract = abstract
     @event = @abstract.event.decorate
 
     I18n.with_locale(@abstract.author.locale) do
-      title = I18n.t 'mailers.abstracts.submission_token.subject',
+      title = I18n.t "mailers.abstracts.#{notification_name}.subject",
                      event: @event.tagline
       mail to: @abstract.author.email,
            subject: title,
-           template_path: 'mailer/abstracts'
+           template_path: 'mailer/abstracts',
+           template_name: notification_name.to_s
     end
   end
 end
